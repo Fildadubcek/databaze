67844d125bb301a42c999ed78c2476a7395924e7
Fildadubcek <dubcekf@gmail.com>
2021-09-24 12:15:37 +0200

    carshop.sql

--- carshop.sql
+++ carshop.sql
@@ -5,3 +5,11 @@ CREATE TABLE car (
  manufacturer VARCHAR (255),
  model VARCHAR (255)
 );
+
+DESCRIBE car 
+;
+# vkládání řádků do tabulky
+INSERT INTO car (id, model, manufacturer)
+VALUES (' ', ' ', ' ';
+SELECT * FROM car;
+DROP TABLE car;
